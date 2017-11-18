e74fd0da6579cc80933fc530218159fb1c602e6f
Ethan Brooks <ethanabrooks@gmail.com>
2017-11-17 19:50:28 -0500

    added include dir

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 setup(ext_modules = cythonize(
     [
-        Extension('pyx.Landscaping', sources=["pyx/Landscaping.pyx"]), 
+        Extension('pyx.Landscaping', sources=["pyx/Landscaping.pyx"],
+            include_dirs=['pxd']), 
         Extension('pyx.Shrubbing', sources=["pyx/Shrubbing.pyx"])
     ]
     ))
