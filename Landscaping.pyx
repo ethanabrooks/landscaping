773827d6237dc12546274be4fb5b49676e16f13e
Ethan Brooks <ethanabrooks@gmail.com>
2017-11-17 19:10:59 -0500

    still works

--- Landscaping.pyx
+++
@@ -1,6 +0,0 @@
-cimport Shrubbing
-import Shrubbing
-
-cdef Shrubbing.Shrubbery sh
-sh = Shrubbing.standard_shrubbery()
-print "Shrubbery size is %d x %d" % (sh.width, sh.length)
--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@
 
 from distutils.core import setup
 from Cython.Build import cythonize
-setup(ext_modules = cythonize(["Landscaping.pyx", "Shrubbing.pyx"]))
+setup(ext_modules = cythonize(["pyx/Landscaping.pyx", "pyx/Shrubbing.pyx"]))
